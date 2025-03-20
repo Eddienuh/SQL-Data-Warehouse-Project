a8263074211ff2fdd995b6677a4adfadbcb13f7a
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-20 17:58:41 +0000

    Update ddl_Bronze.sql

--- Scripts/Bronze/ddl_Bronze.sql
+++ Scripts/Bronze/ddl_Bronze.sql
@@ -4,30 +4,14 @@ DDL Script: Create Bronze Tables
 =======================================================================================
 
 Script Purpose:
-              This script creates tables in the bronze schema by firstly dropping tables if they already exist, and then recreating tables.
-              This script also utilises bulk insert for each table load, ensuring to truncate each table before the load is executed.
-              The time for each table load and for the batch load are both defined within the script.
-              Run this script to re-define the DDL structure of Bronze tables.  
+		This script creates tables within the Bronze schema, Dropping any previous tables if the already Exists.
+		Run this script to re-define the DDL structure of Bronze tables.
+		Description of events are provided throughout table creation.
 
 =========================================================================================
 */ 
 
-CREATE OR ALTER PROCEDURE Bronze.load_Bronze
-AS
-BEGIN
-	DECLARE @START_TIME DATETIME, @END_TIME DATETIME, @BATCH_START_TIME DATETIME, @BATCH_END_TIME DATETIME;
-	BEGIN TRY
-		SET @BATCH_START_TIME = GETDATE ();
-
-		PRINT '==============================================';
-		PRINT 'Loading Bronze Layer';
-		PRINT '==============================================';
-
-		PRINT '----------------------------------------------';
-		PRINT 'Loading crm Tables';
-		PRINT '----------------------------------------------';
-
-		PRINT '>> CHECK AND DROP Bronze.crm_cust_info IF TABLE EXISTS'; 
+PRINT '>> CHECK AND DROP Bronze.crm_cust_info IF TABLE EXISTS'; 
 		IF OBJECT_ID('Bronze.crm_cust_info', 'U') IS NOT NULL
 		BEGIN
 			DROP TABLE Bronze.crm_cust_info;
@@ -125,142 +109,4 @@ BEGIN
 		CAT NVARCHAR (50),
 		SUBCAT NVARCHAR (50),
 		MAINTENANCE NVARCHAR (50),
-		);
-
-		PRINT '-----------------------------------------------------';
-		PRINT ' TRUNCATE AND BULK INSERT DATA INTO TABLES';
-		PRINT '-----------------------------------------------------';
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.crm_cust_info';
-		TRUNCATE TABLE Bronze.crm_cust_info
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.crm_cust_info';
-		BULK INSERT Bronze.crm_cust_info
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\cust_info.csv'
-
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			ROWTERMINATOR = '\n', 
-			TABLOCK
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.crm_prd_info';
-		TRUNCATE TABLE Bronze.crm_prd_info
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.crm_prd_info';
-		BULK INSERT Bronze.crm_prd_info 
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\prd_info.csv'
-
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			ROWTERMINATOR = '\n',
-			TABLOCK
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.crm_sales_details';
-		TRUNCATE TABLE Bronze.crm_sales_details;
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.crm_sales_details'; 
-		BULK INSERT Bronze.crm_sales_details
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\sales_details.csv'
-
-		WITH (
-			FIRSTROW = 2,             
-			FIELDTERMINATOR = ',',    
-			ROWTERMINATOR = '\n',     
-			TABLOCK                  
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.erp_CUST_AZ12';
-		TRUNCATE TABLE Bronze.erp_CUST_AZ12
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.erp_CUST_AZ12';
-		BULK INSERT Bronze.erp_CUST_AZ12
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\CUST_AZ12.csv'
-
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR =',',
-			ROWTERMINATOR = '\n', 
-			TABLOCK
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.erp_LOC_A1O1';
-		TRUNCATE TABLE Bronze.erp_LOC_A1O1;
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.erp_LOC_A1O1';
-		BULK INSERT Bronze.erp_LOC_A1O1
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\LOC_A101.csv'
-
-		WITH (
-			FIRSTROW = 2, 
-			FIELDTERMINATOR = ',',
-			ROWTERMINATOR = '\n',      
-			TABLOCK                   
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-
-		SET @START_TIME = GETDATE ();
-		PRINT '>> TRUNCATE TABLE: Bronze.erp_PX_CAT_G1V2';
-		TRUNCATE TABLE Bronze.erp_PX_CAT_G1V2;
-
-		PRINT '>> BULK INSERT DATA INTO TABLE Bronze.erp_PX_CAT_G1V2';
-		BULK INSERT Bronze.erp_PX_CAT_G1V2
-
-		FROM 'C:\Users\eddie\OneDrive\Documents\PX_CAT_G1V2.csv'
-
-		WITH (
-			FIRSTROW = 2,            
-			FIELDTERMINATOR = ',',   
-			ROWTERMINATOR = '\n',    
-			TABLOCK                 
-		);
-		SET @END_TIME = GETDATE ();
-		PRINT 'LOAD DURATION: ' + CAST(DATEDIFF (SECOND, @START_TIME, @END_TIME) AS NVARCHAR) + ' SECONDS';
-		PRINT '------------------------------------------------------'
-
-		SET @BATCH_END_TIME = GETDATE ();
-		PRINT '======================================================'
-		PRINT 'LOADING BRONZE LAYER IS COMPLETE';
-		PRINT ' - TOTAL DURATION: ' + CAST( DATEDIFF (SECOND, @BATCH_START_TIME, @BATCH_END_TIME) AS NVARCHAR) + ' SECONDS'; 
-		PRINT '======================================================'
-
-	END TRY 
-	BEGIN CATCH
-		PRINT '======================================================='
-		PRINT 'ERROR MESSAGE OCCURED WHULE LOADING BRONZE LAYER'
-		PRINT 'ERROR MESSAGE' + ERROR_MESSAGE ();
-		PRINT 'ERROR MESSAGE' + CAST (ERROR_NUMBER () AS NVARCHAR);
-		PRINT 'ERROR MESSAGE' + CAST (ERROR_STATE () AS NVARCHAR);
-		PRINT '======================================================='
-	END CATCH
-END 
+		); 
