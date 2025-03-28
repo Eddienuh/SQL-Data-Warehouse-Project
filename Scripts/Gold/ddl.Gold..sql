a05c1ab048f552b85234aec954cc7827234a2912
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-28 17:58:59 +0000

    Update ddl.Gold..sql

--- Scripts/Gold/ddl.Gold..sql
+++ Scripts/Gold/ddl.Gold..sql
@@ -13,29 +13,37 @@ Usage:
 ======================================================================================================
 */
 
-
+-- Drop and recreate Gold.dim_customers view
+IF OBJECT_ID('Gold.dim_customers', 'V') IS NOT NULL
+	DROP VIEW Gold.dim_customers;
+GO
 
 CREATE VIEW Gold.dim_customers AS
 SELECT
-	ROW_NUMBER () OVER (ORDER BY cust_id) AS customer_key, 
+	ROW_NUMBER () OVER (ORDER BY ci.cust_id) AS customer_key, 
 	ci.cust_id AS customer_id,
 	ci.cust_key AS customer_number,
 	ci.cust_firstname AS first_name,
 	ci.cust_lastname AS last_name,
 	la.CNTRY AS country,
 	CASE 
-	WHEN ci.cust_gender ! = 'unknown' THEN ci.cust_gender --CRM is the master for gender information
-	 ELSE COALESCE (ca.GEN, 'Unknown') 
-END AS gender, --Intergrate new gender information
+		WHEN ci.cust_gender <> 'unknown' THEN ci.cust_gender --CRM is the master for gender 
+		ELSE COALESCE(ca.GEN, 'Unknown') 
+	END AS gender, -- Integrate new gender information
 	ci.cust_marital_status AS marital_status,
 	ca.BDATE AS birthdate,
 	ci.cust_create_date AS create_date
 FROM Silver.crm_cust_info ci
 LEFT JOIN Silver.erp_CUST_AZ12 ca
-ON		ci.cust_key = ca.CID
+	ON ci.cust_key = ca.CID
 LEFT JOIN Silver.erp_LOC_A1O1 la
-ON		ci.cust_key = la.CID 
+	ON ci.cust_key = la.CID;
+GO
 
+-- Drop and recreate Gold.dim_products view
+IF OBJECT_ID('Gold.dim_products', 'V') IS NOT NULL
+	DROP VIEW Gold.dim_products;
+GO
 
 CREATE VIEW Gold.dim_products AS
 SELECT
@@ -52,9 +60,14 @@ SELECT
 	pn.prd_start_date AS start_date
 FROM Silver.crm_prd_info pn
 LEFT JOIN Silver.erp_PX_CAT_G1V2 pc
-ON pn.cat_id = pc.id
-WHERE prd_end_date IS NULL --Filter out all historical data 
+	ON pn.cat_id = pc.id
+WHERE pn.prd_end_date IS NULL; -- Filter out all historical data
+GO
 
+-- Drop and recreate Gold.fact_sales view
+IF OBJECT_ID('Gold.fact_sales', 'V') IS NOT NULL
+	DROP VIEW Gold.fact_sales;
+GO
 
 CREATE VIEW Gold.fact_sales AS
 SELECT
@@ -69,8 +82,7 @@ SELECT
 	sd.sls_price AS price
 FROM Silver.crm_sales_details sd
 LEFT JOIN Gold.dim_products pr
-ON sls_prd_key = pr.product_number
+	ON sd.sls_prd_key = pr.product_number  
 LEFT JOIN Gold.dim_customers cu
-ON sd.sls_cust_id = cu.customer_id
- 
-
+	ON sd.sls_cust_id = cu.customer_id;
+GO 
