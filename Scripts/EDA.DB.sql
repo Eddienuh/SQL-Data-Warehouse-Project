3882b692a9d53b9f953ecf4e315ad6b5f8feffb5
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-29 12:51:58 +0000

    Update EDA.DB.sql

--- Scripts/EDA.DB.sql
+++ Scripts/EDA.DB.sql
@@ -1,9 +1,21 @@
-PRINT '-----------------------------------------------------'
-PRINT 'Explore various tables and coluns within the database'
-PRINT '-----------------------------------------------------'
+PRINT '-----------------------------------------------------';
+PRINT 'Explore various tables and coluns within the database';
+PRINT '-----------------------------------------------------';
+
 --Explore all objects in Database
 SELECT * FROM INFORMATION_SCHEMA.TABLES
 
 --Explore all columns in Database
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'dim_customers' --exploration example 
+
+PRINT '------------------------------------------------------------------';
+PRINT 'Explore country and category dimensions';
+PRINT '------------------------------------------------------------------';
+
+--Explore all countries our customers come from 
+SELECT DISTINCT country FROM Gold.dim_customers 
+
+--Explore all categories "The Major Divisions"
+SELECT DISTINCT category, subcategory, product_name FROM Gold.dim_products 
+ORDER BY 1, 2, 3
