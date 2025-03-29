d4baa1232db3328af87b54ca72f883d497e353f4
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-29 13:15:15 +0000

    Update EDA.DB.sql

--- Scripts/EDA.DB.sql
+++ Scripts/EDA.DB.sql
@@ -19,3 +19,26 @@ SELECT DISTINCT country FROM Gold.dim_customers
 --Explore all categories "The Major Divisions"
 SELECT DISTINCT category, subcategory, product_name FROM Gold.dim_products 
 ORDER BY 1, 2, 3
+
+PRINT '------------------------------------------------------------------';
+PRINT 'Explore Date Boundaries and Scope';
+PRINT '------------------------------------------------------------------';
+
+--Find the date of the first and last order
+--Find how many years of sales data is available 
+
+SELECT 
+	MIN(order_date) AS first_order,
+	MAX(order_date) AS last_order,
+	DATEDIFF(YEAR, MIN(order_date), MAX(order_date)) AS order_range_years,
+	DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS order_range_months
+	FROM Gold.fact_sales
+
+
+--Find the youngest and the oldest customer
+SELECT
+	MIN(birthdate) AS oldest_customer,
+	DATEDIFF (YEAR, MIN(birthdate), GETDATE ()) AS oldest_customer_age,
+	MAX(birthdate) AS youngest_customer,
+	DATEDIFF (YEAR, MAX(birthdate), GETDATE ()) AS youngest_customer_age
+	FROM Gold.dim_customers
