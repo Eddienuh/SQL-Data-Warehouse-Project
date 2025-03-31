6e8817a59325e269096d5cd9ac01511436c9f416
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-31 21:27:32 +0100

    Update ADA.DB.sql

--- Scripts/ADA.DB.sql
+++ Scripts/ADA.DB.sql
@@ -5,13 +5,17 @@ Adavanced Data Analytics
 Script Purpose:
         This script progresses on from the EDA script, providing more advanced queries to
         further analyse our data in order to uncover trends in our business over time.
+
+Usage:  Queries can be adjusted using appropriate window functions to analyse business trends 
+        and support informed decision-making.
 */
 
-
-
+=======================================================================================
+--CHANGE-OVER-TIME (TRENDS)
+========================================================================================
 --Track trends and uncover seasonality in our data over time
-
---Solution 1
+        
+--Solution 1        
 SELECT
 YEAR(order_date) AS order_year,
 MONTH(order_date) AS order_month, 
@@ -45,3 +49,57 @@ FROM Gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY FORMAT(order_date, 'yyyy-MMM')
 ORDER BY FORMAT(order_date, 'yyyy-MMM')
+
+========================================================================
+--CUMULATIVE ANALYSIS
+========================================================================
+--Calculate total sales per month
+--and the running total of sales over time
+
+--Value of total sales month on month
+SELECT
+order_date,
+total_sales,
+SUM(total_sales) OVER (PARTITION BY order_date ORDER BY order_date) AS running_total_sales --Window function for cumalitive measure
+FROM 
+(
+	SELECT 
+	DATETRUNC(MONTH, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales
+	FROM Gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+)t
+
+--Cumulative value of total sales year on year
+SELECT
+order_date,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales --Window function for cumalitive measure
+FROM 
+(
+	SELECT 
+	DATETRUNC(YEAR, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales
+	FROM Gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(YEAR, order_date)
+)t
+
+
+--Moving average of total sales year on year
+SELECT
+order_date,
+total_sales,
+SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,--Window function for cumalitive measure
+AVG(avg_price) OVER (ORDER BY order_date) AS moving_avg_price     --Window function for moving average
+FROM 
+(
+	SELECT 
+	DATETRUNC(YEAR, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	AVG(price) AS avg_price
+	FROM Gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(YEAR, order_date)
+)t
