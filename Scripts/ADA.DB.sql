56b1533081390f64d29162c7ebc781d3f1adbcf0
Eddienuh <eddienuh@yahoo.co.uk>
2025-03-31 22:46:37 +0100

    Update ADA.DB.sql

--- Scripts/ADA.DB.sql
+++ Scripts/ADA.DB.sql
@@ -105,3 +105,56 @@ FROM
 	WHERE order_date IS NOT NULL
 	GROUP BY DATETRUNC(YEAR, order_date)
 )t
+
+
+------------------------------------------------------------------
+--PERFORMANCE ANALYSIS (YEAR ON YEAR)
+------------------------------------------------------------------
+
+--Task:Analyse the yearly performance of products by comparing each products
+--sales to both its average sales performance and previous years sales
+
+--1. Find the current yearly sales for each product 
+
+WITH yearly_products_sales AS (
+	SELECT
+	YEAR(f.order_date) AS order_year,
+	p.product_name,
+	SUM(f.sales_amount) AS current_sales
+	FROM Gold.fact_sales f
+	LEFT JOIN Gold.dim_products p 
+	ON f.product_key = p.product_key
+	WHERE order_date IS NOT NULL
+	GROUP BY
+	YEAR(f.order_date),
+	p.product_name
+)
+
+	
+--2. Find the average yearly sales for each product 
+SELECT 
+	order_year,
+	product_name,
+	current_sales,
+	AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales,
+	current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
+	CASE 
+		WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0
+		THEN 'Above Avg'
+		WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0
+		THEN 'Below Avg'
+		ELSE 'Avg'
+	END avg_change,
+
+--2b. Compare current years sales to previous year sales
+	LAG (current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS prev_yr_sales,
+	current_sales - LAG (current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS diff_per_yr,
+	CASE 
+		WHEN current_sales - LAG (current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0
+		THEN 'Increase'
+		WHEN current_sales - LAG (current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0
+		THEN 'Decrease'
+		ELSE 'Unchanged'
+	END prev_yr_change
+	FROM yearly_products_sales
+	ORDER BY product_name, order_year
